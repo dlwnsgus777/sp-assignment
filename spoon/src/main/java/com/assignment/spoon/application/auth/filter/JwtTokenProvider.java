91813382932208ad4e3b79fad285c1c1fcfc95a7
Ted <dlwnsgus777@naver.com>
2023-08-24 09:17:23 +0900

    3. jwt token ing...
    3. jwt token ing...

---
+++ spoon/src/main/java/com/assignment/spoon/application/auth/filter/JwtTokenProvider.java
@@ -0,0 +1,68 @@
+package com.assignment.spoon.application.auth.filter;
+
+import com.assignment.spoon.application.auth.CustomUserDetailService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+   private final String secretKey;
+
+   private final long validityInMilliseconds;
+   private final CustomUserDetailService userDetailService;
+
+   public JwtTokenProvider(
+         @Value("${security.jwt.token.secret-key}") String secretKey,
+         @Value("${security.jwt.token.expire-length}") long validityInMilliseconds,
+         CustomUserDetailService userDetailService) {
+      this.secretKey = secretKey;
+      this.validityInMilliseconds = validityInMilliseconds;
+      this.userDetailService = userDetailService;
+   }
+
+   public String createToken(Authentication authentication) {
+      Claims claims = Jwts.claims().setSubject(authentication.getName());
+      Date now = new Date();
+      Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+      return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+   }
+
+   public boolean validateToken(String token) {
+      try {
+         Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+         return !claims.getBody().getExpiration().before(new Date());
+      } catch (IllegalArgumentException e) {
+         return false;
+      }
+   }
+
+   public Authentication getAuthentication(String token) {
+      UserDetails user = userDetailService.loadUserByUsername(getPayload(token));
+      return new UsernamePasswordAuthenticationToken(user, "", user.getAuthorities());
+   }
+
+   public String extractToken(HttpServletRequest request) {
+      return new JwtTokenExtractor(request).extract();
+   }
+
+   private String getPayload(String token) {
+      return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+   }
+}
